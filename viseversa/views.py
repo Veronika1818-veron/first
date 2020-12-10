efb7ceaf5fd37d623253bd35490f35cff6b981eb
VeronikaShatalova <veronika.shatalova.veronika@gmail.com>
2020-12-10 13:15:17 +0200

    Add new new

--- viseversa/views.py
+++ viseversa/views.py
@@ -5,5 +5,7 @@ def home(request):
 
 def reverse(request):
 	test_get_text = request.GET['usertext']
+	words = test_get_text.split()
+	number_of_words = len(words)
 	reversed_text = test_get_text[::-1]
-	return render(request, 'reverse.html', {'test_get_text':test_get_text, 'reversedtext': reversed_text})
+	return render(request, 'reverse.html', {'test_get_text':test_get_text, 'reversedtext': reversed_text, 'number_of_words': number_of_words})
