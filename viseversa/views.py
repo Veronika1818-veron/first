9d1269218a640fa4138870c5e9e6551059c9ee76
VeronikaShatalova <veronika.shatalova.veronika@gmail.com>
2020-12-10 12:56:49 +0200

    Add new

--- viseversa/views.py
+++ viseversa/views.py
@@ -1,3 +1,9 @@
 ﻿from django.shortcuts import render
+
 def home(request):
 	return render(request, 'home.html')
+
+def reverse(request):
+	test_get_text = request.GET['usertext']
+	reversed_text = test_get_text[::-1]
+	return render(request, 'reverse.html', {'test_get_text':test_get_text, 'reversedtext': reversed_text})
